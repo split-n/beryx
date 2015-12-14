14716df4fb18895708ac2c36a3b28a009babe02f
s <git@tnu.jp>
2015-12-15 00:35:02 +0900

    CrawlDirectory first test

--- beryx_app/test/models/crawl_directory_test.rb
+++ beryx_app/test/models/crawl_directory_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CrawlDirectoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid with not existed directory path" do
+    Dir.stub(:exists?, false) do
+      cd = CrawlDirectory.create(path: "/nowhere")
+      assert cd.invalid?
+    end
+  end
+
+  test "valid with existed directory path" do
+    Dir.stub(:exists?, true) do
+      cd = CrawlDirectory.create(path: "/found")
+      assert cd.valid?
+    end
+  end
 end
