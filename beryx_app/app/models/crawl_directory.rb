eac92d5d5429a62edd66ee1d255f2e423ba8d585
s <git@tnu.jp>
2015-12-14 23:47:19 +0900

    Validate CrawlDirectory#path exists

--- beryx_app/app/models/crawl_directory.rb
+++ beryx_app/app/models/crawl_directory.rb
@@ -1,2 +1,11 @@
 class CrawlDirectory < ActiveRecord::Base
+  validates :path, presence: true
+  validate :path_should_exists
+
+  private
+  def path_should_exists
+    unless Dir.exists?(path)
+      errors.add(:path, "Path directory not found.")
+    end
+  end
 end
