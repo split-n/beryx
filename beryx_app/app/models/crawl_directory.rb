c08f3ef76faa692bcfd749a02a01a6b2aeb01659
s <git@tnu.jp>
2015-12-15 13:00:22 +0900

    CrawlDirectory test, validate

--- beryx_app/app/models/crawl_directory.rb
+++ beryx_app/app/models/crawl_directory.rb
@@ -1,6 +1,7 @@
 class CrawlDirectory < ActiveRecord::Base
-  validates :path, presence: true
+  validates :path, presence: true, format: {with: %r{\A/.*/\z}, message: "Path should start /, ending / ." }
   validate :path_should_exists
+  validate :path_should_not_include_others
   has_many :videos, dependent: :destroy
 
   private
@@ -8,5 +9,18 @@ class CrawlDirectory < ActiveRecord::Base
     unless path.present? && Dir.exists?(path)
       errors.add(:path, "Path directory not found.")
     end
+  end
+
+  def path_should_not_include_others
+    all = CrawlDirectory.all
+    included = all.reject{|d| d.id == self.id }
+                   .select{|d| dir_includes?(d, self)}
+    unless included.empty?
+      errors.add(:path, "Path duplicated with #{included.first.path}")
+    end
+  end
+
+  def dir_includes?(d1, d2)
+    d1.path.start_with?(d2.path) || d2.path.start_with?(d1.path)
   end
 end
--- beryx_app/test/models/crawl_directory_test.rb
+++ beryx_app/test/models/crawl_directory_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class CrawlDirectoryTest < ActiveSupport::TestCase
   test "invalid with not existed directory path" do
-    Dir.stub(:exists?, false) do
-      cd = CrawlDirectory.create(path: "/nowhere")
+    stub_exists(false) do
+      cd = CrawlDirectory.create(path: "/nowhere/")
       assert cd.invalid?
     end
   end
@@ -17,9 +17,53 @@ class CrawlDirectoryTest < ActiveSupport::TestCase
   end
 
   test "valid with existed directory path" do
-    Dir.stub(:exists?, true) do
-      cd = CrawlDirectory.create(path: "/found")
+    stub_exists do
+      cd = CrawlDirectory.create(path: "/found/")
       assert cd.valid?
+    end
+  end
+
+  test "invalid when path isn't ended with slash" do
+    stub_exists do
+      cd = CrawlDirectory.create(path: "/foo")
+      assert cd.invalid?
+    end
+  end
+
+  test "invalid when path isn't started with slash" do
+    stub_exists do
+      cd = CrawlDirectory.create(path: "foo/")
+      assert cd.invalid?
+    end
+  end
+
+  test "invalid when other directory is included" do
+    stub_exists do
+      d1 = CrawlDirectory.create(path: "/foo/bar/")
+      d2 = CrawlDirectory.create(path: "/foo/")
+      assert d2.invalid?
+    end
+  end
+
+  test "invalid when other directory includes this" do
+    stub_exists do
+      d1 = CrawlDirectory.create(path: "/foo/bar/")
+      d2 = CrawlDirectory.create(path: "/foo/bar/baz/")
+      assert d2.invalid?
+    end
+  end
+
+  test "invalid when same directory found" do
+    stub_exists do
+      d1 = CrawlDirectory.create(path: "/foo/bar/")
+      d2 = CrawlDirectory.create(path: "/foo/bar/")
+      assert d2.invalid?
+    end
+  end
+
+  def stub_exists(result=true)
+    Dir.stub(:exists?, result) do
+      yield
     end
   end
 end
