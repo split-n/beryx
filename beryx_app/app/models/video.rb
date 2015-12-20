f478b282e541000afebe8ab76f69978075cd0e1a
s <git@tnu.jp>
2015-12-20 23:41:45 +0900

    video++

--- beryx_app/app/models/video.rb
+++ beryx_app/app/models/video.rb
@@ -1,27 +1,28 @@
 class Video < ActiveRecord::Base
-  VIDEO_EXTS = %w(mp4 mkv) # temp
+  VIDEO_EXTS = %w(.mp4 .mkv) # temp
   belongs_to :crawl_directory
   validates :crawl_directory, presence: true
   validates :path, presence: true
   validate :path_should_exists
+  validate :path_file_extension
 
   before_save do
-    file_name = File.basename(path)
-    file_size = File.size(path)
+    self.file_name = File.basename(path)
+    self.file_size ||= File.size(path)
   end
 
   private
   def path_should_exists
-    unless path.present? && File.exists?(path)
+    unless path.present? && File.exist?(path)
       errors.add(:path, "Path file not found.")
     end
   end
 
   def path_file_extension
-    VIDEO_EXTS.each do |ext|
-      unless path.downcase.end_with? ext
-        errors.add(:path, "File #{File.extname(path)} is not supported.")
-      end
+    return if path.blank?
+    ext = File.extname(path)
+    unless ext.in?(VIDEO_EXTS)
+      errors.add(:path, "File #{ext} is not supported.")
     end
   end
 end
--- beryx_app/spec/models/video_spec.rb
+++ beryx_app/spec/models/video_spec.rb
@@ -1,13 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Video, type: :model do
-  let(:cd_path) { "/exists/" }
-  before { allow(Dir).to receive(:exist?).with(cd_path).and_return(true) }
-  let!(:cd) { CrawlDirectory.create(path: cd_path) }
   describe "new instance" do
-    context "without args" do
-      subject{ cd.videos.create }
-      it { is_expected.not_to be_valid }
+    context "without CrawlDirectory" do
+      context "cd is nil" do
+        let(:path) { "/exists/foo.mp4" }
+        before { allow(Dir).to receive(:exist?).with(path).and_return(true) }
+        subject { Video.create(path: path) }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "specify invalid cd_id" do
+        let(:path) { "/exists/foo.mp4" }
+        before { allow(Dir).to receive(:exist?).with(path).and_return(true) }
+        subject { Video.create(path: path, crawl_directory_id: 4) }
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    context "with CrawlDirectory" do
+      let(:cd_path) { "/exists/" }
+      before { allow(Dir).to receive(:exist?).with(cd_path).and_return(true) }
+      let!(:cd) { CrawlDirectory.create(path: cd_path) }
+
+      context "without args" do
+        subject{ cd.videos.create }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "with not exist path" do
+        let(:path) { "/exists/nil.mp4" }
+        before { allow(File).to receive(:exist?).with(path).and_return(false) }
+        subject{ cd.videos.create(path: path) }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "with exist path" do
+        let(:path) { "/exists/foo.mp4" }
+        context "without file_size" do
+          before {
+            allow(File).to receive(:exist?).with(path).and_return(true)
+            allow(File).to receive(:size).with(path).and_return(350*1024**2)
+          }
+          subject{ cd.videos.create(path: path) }
+          it { is_expected.to be_valid }
+        end
+
+        context "with file_size" do
+          before {
+            allow(File).to receive(:exist?).with(path).and_return(true)
+          }
+          subject{ cd.videos.create(path: path, file_size: 350*1024**2) }
+          it { is_expected.to be_valid }
+        end
+      end
     end
   end
 end
