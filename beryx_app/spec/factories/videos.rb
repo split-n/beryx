fd64274fcb5e261bd3e64fa86e4451a70dcc4a87
s <git@tnu.jp>
2016-01-05 11:57:28 +0900

    video factory to work

--- beryx_app/spec/factories/videos.rb
+++ beryx_app/spec/factories/videos.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
-  after(:build) do |video|
-    allow(File).to receive(:exist?).with(video.path).and_return(true)
-  end
-
   factory :video do
-    path "/valid/foo/bar.mp4"
+    after(:build) do |video|
+      allow(File).to receive(:exist?).with(video.path).and_return(true)
+    end
+    crawl_directory
+    sequence(:path) {|n| "#{crawl_directory.path}foo#{n}.mp4" }
     file_size 200.megabytes
   end
 end
