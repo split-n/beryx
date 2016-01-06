1ef15397f7fb6be1f4821bcfa3ba72fdc0fe8c36
s <git@tnu.jp>
2016-01-06 16:05:57 +0900

    user more spec

--- beryx_app/app/models/user.rb
+++ beryx_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   validates :login_id, presence: true, format: { with: /\A\w{3,20}\z/ }
+  validates :password, length: { minimum: 7 }
   has_secure_password
 end
--- beryx_app/spec/models/user_spec.rb
+++ beryx_app/spec/models/user_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "on create" do
-    subject{ User.create(login_id: login_id, password: password, password_confirmation: password_confirmation) }
     describe "login_id" do
       let(:password) { "password" }
-      let(:password_confirmation) { "password" }
+      subject{ User.create(login_id: login_id, password: password) }
 
       context "length is 3" do
         let(:login_id) { "foo" }
@@ -51,6 +50,57 @@ RSpec.describe User, type: :model do
         let(:login_id) { "12345" }
         it { is_expected.to be_valid}
       end
+    end
+
+    describe "password" do
+      let(:login_id) { "foobar" }
+      subject{ User.create(login_id: login_id, password: password ) }
+
+      context "is not set" do
+        let(:password) { nil }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "length is 6" do
+        let(:password) { "a"*6 }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "length is 7" do
+        let(:password) { "a"*7 }
+        it { is_expected.to be_valid }
+      end
+
+    end
+
+    describe "password confirmation" do
+      let(:login_id) { "foobar" }
+      subject{ User.create(login_id: login_id, password: password, password_confirmation: password_confirmation) }
+
+      context "is valid" do
+        let(:password) { "password" }
+        let(:password_confirmation) { password }
+        it { is_expected.to be_valid }
+      end
+
+      context "is not set" do
+        let(:password) { "password" }
+        let(:password_confirmation) { nil }
+        it { is_expected.to be_valid }
+      end
+
+      context "is set but password is not set" do
+        let(:password) { nil }
+        let(:password_confirmation) { "password" }
+        it { is_expected.not_to be_valid }
+      end
+
+      context "is differ" do
+        let(:password) {  "password" }
+        let(:password_confirmation) { "passxxxx" }
+        it { is_expected.not_to be_valid }
+      end
+
     end
   end
 end
