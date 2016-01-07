ef8beaaa2492582c422377ffcb13df5914f3bbf3
s <git@tnu.jp>
2016-01-08 01:06:25 +0900

    SessionsController spec

--- beryx_app/spec/controllers/sessions_controller_spec.rb
+++ beryx_app/spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:user) { FG.create(:user) }
+  let(:user_admin) { FG.create(:user_admin) }
 
+  describe "#new" do
+    it "normal" do
+       get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "#create" do
+    it "normal user login" do
+      post :create, user: { login_id: user.login_id, password: user.password }
+      expect(response).to redirect_to "/"
+      expect(session[:user_id]).to eq user.id
+    end
+
+    it "admin user login" do
+      post :create, user: { login_id: user_admin.login_id, password: user_admin.password }
+      expect(response).to redirect_to "/"
+      expect(session[:user_id]).to eq user_admin.id
+    end
+
+    it "wrong input" do
+      post :create, user: { login_id: "foobar", password: "12345678" }
+      expect(response).to render_template :new
+      expect(session[:user_id]).to eq nil
+    end
+  end
 end
