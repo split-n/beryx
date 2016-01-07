832fb9ca820cea4a0a9fd9de9fb3a699fdb000ed
s <git@tnu.jp>
2016-01-07 13:59:02 +0900

    login form

--- beryx_app/app/controllers/sessions_controller.rb
+++ beryx_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(login_id: params[:user][:login_id])
+    if user&.authenticate(params[:user][:password])
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
