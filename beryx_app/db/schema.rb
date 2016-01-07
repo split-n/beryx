ed5125656e1fea1d23a4c74ff800ebe74ccb4c7f
s <git@tnu.jp>
2016-01-07 09:49:23 +0900

    add admin? col on users

--- beryx_app/db/schema.rb
+++ beryx_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106060746) do
+ActiveRecord::Schema.define(version: 20160107004533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,9 @@ ActiveRecord::Schema.define(version: 20160106060746) do
   create_table "users", force: :cascade do |t|
     t.string   "login_id"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false, null: false
   end
 
   create_table "videos", force: :cascade do |t|
