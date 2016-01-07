ff17f2579f7832fc77022795ebf1a4ba69964043
s <git@tnu.jp>
2016-01-07 13:06:16 +0900

    create SessionsController

--- beryx_app/config/routes.rb
+++ beryx_app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :crawl_directories, only: [:index, :show, :new, :create, :destroy]
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
