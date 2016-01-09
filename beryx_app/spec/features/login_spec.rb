cda4b43a5aeef91993bf7463eb88336c590a6c32
s <git@tnu.jp>
2016-01-09 16:00:03 +0900

    more login feature spec

--- beryx_app/spec/features/login_spec.rb
+++ beryx_app/spec/features/login_spec.rb
@@ -2,11 +2,27 @@ require 'rails_helper'
 
 RSpec.feature "login", type: :feature do
   let!(:user) { FG.create(:user) }
-  it "wrong username" do
+  it "with wrong username" do
     visit login_path
     fill_in "user[login_id]", with: "foobar"
     fill_in "user[password]", with: user.password
     click_on "Login"
     expect(current_path).to eq login_path
+  end
+
+  it "with wrong password" do
+    visit login_path
+    fill_in "user[login_id]", with: user.login_id
+    fill_in "user[password]", with: "pass12345"
+    click_on "Login"
+    expect(current_path).to eq login_path
+  end
+
+  it "with correct user" do
+    visit login_path
+    fill_in "user[login_id]", with: user.login_id
+    fill_in "user[password]", with: user.password
+    click_on "Login"
+    expect(current_path).to eq root_path
   end
 end
