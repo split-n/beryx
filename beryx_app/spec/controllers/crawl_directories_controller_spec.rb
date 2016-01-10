61e1765822eb11b3d66904af16eb31a3a5c27e73
s <git@tnu.jp>
2016-01-10 20:01:47 +0900

    crawl_directories_controller_spec

--- beryx_app/spec/controllers/crawl_directories_controller_spec.rb
+++ beryx_app/spec/controllers/crawl_directories_controller_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CrawlDirectoriesController, type: :controller do
+  describe "#index" do
+    shared_examples "forbidden" do
+      it "redirected to login" do
+        get :index
+        expect(response).to redirect_to login_path
+      end
+    end
 
+    context "not logged" do
+      it_behaves_like "forbidden"
+    end
 
+    context "logged with normal user" do
+      let(:user) { FG.create(:user) }
+      before { log_in_as(user) }
+      it_behaves_like "forbidden"
+    end
+
+    context "logged with admin user" do
+      let(:user) { FG.create(:user_admin) }
+      before { log_in_as(user) }
+      it "can get" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+  end
 end
