5a5d275866d6844263636916e5fd744526a8ee30
s <git@tnu.jp>
2016-01-11 23:40:02 +0900

    refactor and add crawl_directories_controller_spec

--- beryx_app/spec/controllers/crawl_directories_controller_spec.rb
+++ beryx_app/spec/controllers/crawl_directories_controller_spec.rb
@@ -1,31 +1,82 @@
 require 'rails_helper'
 
 RSpec.describe CrawlDirectoriesController, type: :controller do
-  describe "#index" do
-    shared_examples "forbidden" do
-      it "redirected to login" do
-        get :index
-        expect(response).to redirect_to login_path
-      end
+  shared_examples "login_required" do
+    it "redirected to login" do
+      subject
+      expect(response).to redirect_to login_path
     end
+  end
+
+  shared_context "not_logged" do
+  end
+
+  shared_context "logged_normal_user" do
+    let(:user) { FG.create(:user) }
+    before { log_in_as(user) }
+  end
+
+  shared_context "logged_admin_user" do
+    let(:user) { FG.create(:user_admin) }
+    before { log_in_as(user) }
+  end
+
+
+
+  describe "#index" do
+    subject {
+      get :index
+    }
 
     context "not logged" do
-      it_behaves_like "forbidden"
+      include_context "not_logged"
+      it_behaves_like "login_required"
     end
 
     context "logged with normal user" do
-      let(:user) { FG.create(:user) }
-      before { log_in_as(user) }
-      it_behaves_like "forbidden"
+      include_context "logged_normal_user"
+      it_behaves_like "login_required"
     end
 
     context "logged with admin user" do
-      let(:user) { FG.create(:user_admin) }
-      before { log_in_as(user) }
+      include_context "logged_admin_user"
       it "can get" do
-        get :index
+        subject
         expect(response).to render_template :index
       end
     end
+  end
+
+  describe "#show" do
+
+    context "invalid id" do
+      subject {
+        get :show, id: 5005
+      }
+
+      context "not logged" do
+        include_context "not_logged"
+        it_behaves_like "login_required"
+      end
+
+      context "logged with normal user" do
+        include_context "logged_normal_user"
+        it_behaves_like "login_required"
+      end
+
+      context "logged with admin user" do
+        include_context "logged_admin_user"
+        it "renders 404" do
+          subject
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+    end
+
+    subject {
+      get :show, id: crawl_directory.id
+    }
+
+
   end
 end
