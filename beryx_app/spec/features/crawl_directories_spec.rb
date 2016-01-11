08654fea87338d851676ce36465e1b9c96fe59dc
s <git@tnu.jp>
2016-01-11 22:35:01 +0900

    crawl_directories_spec

--- beryx_app/spec/features/crawl_directories_spec.rb
+++ beryx_app/spec/features/crawl_directories_spec.rb
@@ -1,36 +1,62 @@
 require 'rails_helper'
 
 RSpec.feature "CrawlDirectories", type: :feature do
+  describe "/crawl_directories" do
+    context "with normal user" do
+      let!(:user) { FG.create(:user) }
+      before {
+        log_in_as(user)
+      }
+      it "redirected to login" do
+        visit crawl_directories_path
+        expect(current_path).to eq login_path
+      end
+    end
 
+    context "with admin user" do
+      let!(:user) { FG.create(:user_admin) }
+      before {
+        log_in_as(user)
+      }
 
+      it "can get page and empty list" do
+        visit crawl_directories_path
+        expect(page).not_to have_content "Destroy"
+        expect(page).to have_content "Crawl Directories"
+      end
 
-  context "with normal user" do
-    let!(:user) { FG.create(:user) }
-    before {
-      log_in_as(user)
-    }
-    it "redirected to login" do
-      visit crawl_directories_path
-      expect(current_path).to eq login_path
+      it "can get list with item" do
+        cd = FG.create(:crawl_directory)
+        visit crawl_directories_path
+        expect(page).to have_content cd.path
+      end
     end
   end
 
-  context "with admin user" do
-    let!(:user) { FG.create(:user_admin) }
-    before {
-      log_in_as(user)
-    }
-
-    it "can get page and empty list" do
-      visit crawl_directories_path
-      expect(page).not_to have_content "Destroy"
-      expect(page).to have_content "Crawl Directories"
+  describe "/crawl_directories/:id" do
+    context "with normal user" do
+      let!(:user) { FG.create(:user) }
+      let!(:cd) { FG.create(:crawl_directory) }
+      before {
+        log_in_as(user)
+      }
+      it "redirected to login" do
+        visit crawl_directory_path(cd)
+        expect(current_path).to eq login_path
+      end
     end
 
-    it "can get list with item" do
-      cd = FG.create(:crawl_directory)
-      visit crawl_directories_path
-      expect(page).to have_content cd.path
+    context "with admin user" do
+      let!(:user) { FG.create(:user_admin) }
+      let!(:cd) { FG.create(:crawl_directory) }
+      before {
+        log_in_as(user)
+      }
+      it "gets" do
+        visit crawl_directory_path(cd)
+        expect(page).to have_content cd.path
+      end
     end
   end
+
 end
