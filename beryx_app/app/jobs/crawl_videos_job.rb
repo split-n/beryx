222d2bfe9bae8af003269e7c8e38c7925c43f5b7
s <git@tnu.jp>
2016-01-12 09:55:04 +0900

    CrawlVideosJob: argument use primitive

--- beryx_app/app/jobs/crawl_videos_job.rb
+++ beryx_app/app/jobs/crawl_videos_job.rb
@@ -1,7 +1,8 @@
 class CrawlVideosJob < ActiveJob::Base
   queue_as :crawl_videos
 
-  def perform(crawl_directory)
+  def perform(crawl_directory_id)
+    crawl_directory = CrawlDirectory.find(crawl_directory_id)
     crawl_directory.crawl_videos_and_create
   end
 end
