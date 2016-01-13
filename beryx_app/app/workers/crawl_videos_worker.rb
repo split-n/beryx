1b0975bfe0087710a3b57673b2605566c56b5156
s <git@tnu.jp>
2016-01-13 14:46:58 +0900

    CrawlVideosWorker: get status

--- beryx_app/app/workers/crawl_videos_worker.rb
+++ beryx_app/app/workers/crawl_videos_worker.rb
@@ -5,20 +5,34 @@ class CrawlVideosWorker
                   unique: :while_executing
 
   class << self
-    def queued?(crawl_directory_id)
-      jobs = if CrawlVideosWorker.respond_to?(:jobs) # on testing
-        CrawlVideosWorker.jobs
-      else
-        Sidekiq::Queue.new("crawl_videos")&.map{|x| x.item}
-      end
+    def status(crawl_directory_id)
+      job = get_queued_by_id(crawl_directory_id)
+      return :queued if job
+
+      worker = get_running_worker_by_id(crawl_directory_id)
+      return :running if worker
+
+      :not_running
+    end
+
+    def get_queued_by_id(crawl_directory_id)
+      jobs = Sidekiq::Queue.new("crawl_videos")&.map{|x| x.item}
       jobs&.select{|job|
         job["class"] == "CrawlVideosWorker" && job["args"] == [crawl_directory_id]
-      }.present?
+      }.first
+    end
+
+    def get_running_worker_by_id(crawl_directory_id)
+      Sidekiq::Workers.new.select{|worker|
+        payload = worker[2]["payload"]
+        payload["queue"] == "crawl_videos" && payload["args"] == [crawl_directory_id]
+      }.first
     end
 
   end
 
   def perform(crawl_directory_id)
+    sleep 60
     crawl_directory = CrawlDirectory.find(crawl_directory_id)
     crawl_directory.crawl_videos_and_create
   end
