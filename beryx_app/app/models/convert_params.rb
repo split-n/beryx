f12321e26099040cdb2d6a3fc33b773ab5038eef
s <git@tnu.jp>
2016-01-29 22:37:06 +0900

    define ConvertParams::Base class, as_json limit

--- beryx_app/app/models/convert_params.rb
+++ beryx_app/app/models/convert_params.rb
@@ -1,21 +1,32 @@
 module ConvertParams
-
-  class CopyHls
+  class Base
     include ActiveModel::Model
+    include ActiveModel::Validations::Callbacks
+
+    class << self
+      def define_property(*props)
+        attr_accessor *props
+        define_method("as_json") do
+          super(only: props.map(&:to_s))
+        end
+      end
+    end
+  end
+
+  class CopyHls < Base
     def to_command(source_path, converted_dir_path, converted_file_path)
       raise if self.invalid?
       %Q(ffmpeg -i "#{source_path}" -acodec copy -vcodec copy -sn -vbsf h264_mp4toannexb -map 0 -f segment -segment_format mpegts -segment_time 5 -segment_list "#{converted_file_path}" -segment_list_flags -cache "#{converted_dir_path}/stream%05d.ts")
     end
   end
 
-  class EncodeAvcAacHls
-    include ActiveModel::Model
+  class EncodeAvcAacHls < Base
     include ActiveModel::Validations::Callbacks
 
     X264_PRESETS = %w(ultrafast superfast veryfast faster fast medium slow slower veryslow placebo)
     X264_TUNES = %w(film animation grain stillimage psnr ssim fastdecode zerolatency)
 
-    attr_accessor :video_kbps, :audio_kbps, :width, :height, :preset, :tune
+    define_property :video_kbps, :audio_kbps, :width, :height, :preset, :tune, :he_aac
     validates :video_kbps, presence: true, numericality: { only_integer: true }
     validates :audio_kbps, presence: true, numericality: { only_integer: true }
     validates :height, presence: true, numericality: { only_integer: true }
@@ -32,7 +43,11 @@ module ConvertParams
       raise if self.invalid?
       cmd = ""
       cmd << %Q(ffmpeg -i "#{source_path}" -filter:v "scale=trunc(oh*a/2)*2:#{@height}" )
-      cmd << %Q( -c:a libfdk_aac -b:a #{@audio_kbps}k -c:v libx264 -preset #{@preset} )
+      cmd << %Q( -c:a libfdk_aac )
+      if @he_aac
+        cmd << %Q( -profile:a aac_he )
+      end
+      cmd << %Q( -b:a #{@audio_kbps}k -c:v libx264 -preset #{@preset} )
       if @tune
         cmd << %Q( -tune #{@tune} )
       end
@@ -41,8 +56,7 @@ module ConvertParams
     end
   end
 
-  class CopyFragmentedMp4
-    include ActiveModel::Model
+  class CopyFragmentedMp4 < Base
     def to_command(source_path, converted_dir_path, converted_file_path)
       raise if self.invalid?
       %Q(ffmpeg -i "#{source_path}" -acodec copy -vcodec copy -movflags frag_keyframe+empty_moov -f mp4 #{converted_file_path})
