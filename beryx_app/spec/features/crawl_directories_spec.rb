34541d84ecae302d66e75b7cffe6670032594e46
s <git@tnu.jp>
2016-01-31 03:27:28 +0900

    refactor

--- beryx_app/spec/features/crawl_directories_spec.rb
+++ beryx_app/spec/features/crawl_directories_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "CrawlDirectories", type: :feature do
-  describe "/crawl_directories" do
+  describe "get /crawl_directories" do
     context "with normal user" do
       let!(:user) { FG.create(:user) }
-      before {
-        log_in_as(user)
-      }
+      before { log_in_as(user) }
       it "redirected to login" do
         visit crawl_directories_path
         expect(current_path).to eq login_path
@@ -15,9 +13,7 @@ RSpec.feature "CrawlDirectories", type: :feature do
 
     context "with admin user" do
       let!(:user) { FG.create(:user_admin) }
-      before {
-        log_in_as(user)
-      }
+      before { log_in_as(user) }
 
       it "can get page and empty list" do
         visit crawl_directories_path
@@ -61,13 +57,11 @@ RSpec.feature "CrawlDirectories", type: :feature do
     end
   end
 
-  describe "/crawl_directories/:id" do
+  describe "get /crawl_directories/:id" do
     context "with normal user" do
       let!(:user) { FG.create(:user) }
       let!(:cd) { FG.create(:crawl_directory) }
-      before {
-        log_in_as(user)
-      }
+      before { log_in_as(user) }
       it "redirected to login" do
         visit crawl_directory_path(cd)
         expect(current_path).to eq login_path
@@ -77,9 +71,7 @@ RSpec.feature "CrawlDirectories", type: :feature do
     context "with admin user" do
       let!(:user) { FG.create(:user_admin) }
       let!(:cd) { FG.create(:crawl_directory) }
-      before {
-        log_in_as(user)
-      }
+      before { log_in_as(user) }
       it "gets" do
         visit crawl_directory_path(cd)
         expect(page).to have_content cd.path
