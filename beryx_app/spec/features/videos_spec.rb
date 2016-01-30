d6ff96c8edfc38cdae1af208c5664221db19f550
s <git@tnu.jp>
2016-01-31 03:53:17 +0900

    videos search spec

--- beryx_app/spec/features/videos_spec.rb
+++ beryx_app/spec/features/videos_spec.rb
@@ -11,16 +11,43 @@ RSpec.feature "Videos", type: :feature do
   context "with normal user" do
     let(:user) { FG.create(:user) }
     before { log_in_as(user) }
-    it "gets empty list" do
+    it "can get empty list" do
       visit root_path
       expect(page).to have_content "Videos"
       expect(page).not_to have_selector "td"
     end
 
-    it "gets videos list" do
+    it "can get videos list" do
       video = FG.create(:video)
       visit root_path
-      expect(page).to have_selector "table td", text: video.file_name
+      expect(page).to have_link video.file_name, href: "/videos/#{video.id}"
+    end
+
+    it "can filter video by file name" do
+      cd = FG.create(:crawl_directory)
+      not_match_videos = [
+          FG.create(:video, crawl_directory: cd, path: "#{cd.path}ほげ.mp4"),
+          FG.create(:video, crawl_directory: cd, path: "#{cd.path}hoge_dir/fuga.mp4"),
+          FG.create(:video, crawl_directory: cd, path: "#{cd.path}ho/ge.mp4"),
+      ]
+      match_videos = [
+          FG.create(:video, crawl_directory: cd, path: "#{cd.path}foo/aＨｏｇｅze.mp4"),
+          FG.create(:video, crawl_directory: cd, path: "#{cd.path}the_hoge_movie.mp4"),
+          FG.create(:video, crawl_directory: cd, path: "#{cd.path}HOGE.mp4"),
+      ]
+
+      visit root_path
+      fill_in "q_normalized_file_name_cont_all", with: "hoGe"
+      click_button "Search"
+
+      not_match_videos.each do |video|
+        expect(page).not_to have_content video.file_name
+      end
+      match_videos.each do |video|
+        expect(page).to have_link video.file_name, href: "/videos/#{video.id}"
+      end
+
+      expect(page).to have_content "#{match_videos.count} videos found"
     end
   end
 end
